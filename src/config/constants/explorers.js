c7ee7f40d5b9df217deb6d8092981082811dc7a5
muse_crypto <muse_crypto@protonmail.com>
2022-10-25 01:44:59 +0200

    added doge

--- src/config/constants/explorers.js
+++ src/config/constants/explorers.js
@@ -1,7 +1,7 @@
 // Utility methods to get explorer links
 
 export const explorers = {
-      25: "https://cronos.crypto.org/explorer/",
+      25: "https://cronos.crypto.org/explorer",
       137: "https://polygonscan.com",
       56: "https://bscscan.com",
       321: "https://explorer.kcc.io/en",
@@ -11,8 +11,9 @@ export const explorers = {
       43114: "https://avascan.info/blockchain/c",
       42220: "https://explorer.celo.org",
       1666600000: "https://explorer.harmony.one",
-      1284: "https://blockscout.moonbeam.network/",
+      1284: "https://blockscout.moonbeam.network",
       25: "https://cronoscan.com",
+      2000: "https://explorer.dogechain.dog"
   };
 export const getAddressLink = (chainId, address) => {
     return explorers[chainId] + "/address/" + address
